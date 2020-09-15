1d613be580042f3945451cab42984fa622e5b162
Philipp <mike.litoris@gmail.com>
2020-09-16 00:14:55 +0200

    Added state and whenReady function in FirebaseMusicSource.kt

---
+++ app/src/main/java/com/plcoding/spotifycloneyt/exoplayer/FirebaseMusicSource.kt
@@ -0,0 +1,54 @@
+package com.plcoding.spotifycloneyt.exoplayer
+
+import com.plcoding.spotifycloneyt.exoplayer.State.*
+
+class FirebaseMusicSource {
+
+    private val onReadyListeners = mutableListOf<(Boolean) -> Unit>()
+
+    private var state: State = STATE_CREATED
+        set(value) {
+            if(value == STATE_INITIALIZED || value == STATE_ERROR) {
+                synchronized(onReadyListeners) {
+                    field = value
+                    onReadyListeners.forEach { listener ->
+                        listener(state == STATE_INITIALIZED)
+                    }
+                }
+            } else {
+                field = value
+            }
+        }
+
+    fun whenReady(action: (Boolean) -> Unit): Boolean {
+        if(state == STATE_CREATED || state == STATE_INITIALIZING) {
+            onReadyListeners += action
+            return false
+        } else {
+            action(state == STATE_INITIALIZED)
+            return true
+        }
+    }
+}
+
+enum class State {
+    STATE_CREATED,
+    STATE_INITIALIZING,
+    STATE_INITIALIZED,
+    STATE_ERROR
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
